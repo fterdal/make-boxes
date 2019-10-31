6dc357782bd44f9a33ff8f655b6cdda94d55a5a7
Finn <finnterdal@gmail.com>
2019-10-31 13:29:41 -0500

    messing with box colors

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 // Write your code in this file!
 /* global makeBox */
 
-makeBox()
+const colors = [
+  '#820263', // purple
+  '#D90368', // dogwood
+  '#EADEDA', // gainsboro
+  '#FFD400', // yellow
+  '#016FB9', // spanish blue
+  '#EDF2F4', // anti-flash white
+  'cyan', // cyan
+]
+
+colors.forEach(color => {
+  makeBox(color, `This  box is ${color}`)
+})
--- utils.js
+++ utils.js
@@ -1,10 +1,21 @@
 // You do not need to edit this file.
 
-function makeBox() {
+//
+function makeBox(color, text) {
   console.log('making a box...')
   const container = document.getElementById('container')
   const newBox = document.createElement('DIV')
-  newBox.innerText = 'New Box'
+  newBox.classList.add('box')
+  // newBox.style.width = '5em';
+  // newBox.style.height = '5em';
+  // newBox.style.border = '1px solid black';
+  if (color) {
+    newBox.style.backgroundColor = color
+  }
+  if (text) {
+    newBox.innerText = text
+    newBox.style.textAlign = 'center';
+  }
   container.appendChild(newBox)
 }
 
