887f02455fbcdaaa29f87e3d3c76d1dbdc8a1235
Finn <finnterdal@gmail.com>
2019-10-31 17:16:52 -0500

    got a first lesson up

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
 // Write your code in this file!
-/* global makeBox */
+const colors = window.colors
+const makeBox = window.makeBox
 
-const colors = [
-  'salmon',
-  'crimson',
-  'tomato',
-  'gold',
-  'papayawhip',
-  'peachpuff',
-  'chartreuse',
-  'palegreen',
-  'aquamarine',
-  'turquoise',
-  'skyblue',
-  'plum',
-  'lavander',
-  'pink',
-  'wheat',
-]
+/* Here are some colors you might make a box:
+salmon
+crimson
+tomato
+gold
+papayawhip
+peachpuff
+chartreuse
+palegreen
+aquamarine
+turquoise
+skyblue
+plum
+lavander
+pink
+wheat
+*/
 
-colors.forEach(color => {
-  makeBox(color, `This  box is ${color}`)
-})
-colors.forEach(color => {
-  makeBox(color, `This  box is ${color}`)
-})
-colors.forEach(color => {
-  makeBox(color, `This  box is ${color}`)
-})
+// colors.forEach(color => {
+//   makeBox(`This  box is ${color}`, color)
+// })
+// colors.forEach(color => {
+//   makeBox(color, `This  box is ${color}`)
+// })
 
+/* EXERCISE 1: Calling a function
+  Here are two functions you can use for this exercise: console.log and makeBox.
 
-// EXERCISE 1: Calling a function
+  Functions need to be "called". We sometimes also say "invoking the function".
+  In JavaScript, calling a function means using parethnesis, like this:
+
+  makeBox()
+
+  If you know the name of the function, calling it is as easy as typing the
+  name, and adding a pair of parentheses after it.
+
+  In addition, you can call a function _with arguments_. This is useful whenever
+  you want to make the function do something specific. Let's call the makeBox
+  function with "Hello World":
+
+  makeBox('Hello World')
+
+  Notice how the argument we are passing to the function goes between the
+  parentheses. In fact, functions can take several arguments. For instance,
+  let's call makeBox with two arguments, some text and a color:
+
+  makeBox('Hello World', 'gold')
+
+  Try these examples for yourself and see what the output looks like.
+*/
+
+makeBox()
+makeBox('Hello World')
+makeBox('Hello World', 'gold')
+
 
 
 // EXERCISE 2: Variables and Types
-
--- utils.js
+++ utils.js
@@ -1,35 +1,31 @@
 // You do not need to edit this file.
 
-// const colors = [
-//   'salmon',
-//   'crimson',
-//   'tomato',
-//   'gold',
-//   'papayawhip',
-//   'peachpuff',
-//   'chartreuse',
-//   'palegreen',
-//   'aquamarine',
-//   'turquoise',
-//   'skyblue',
-//   'plum',
-//   'lavander',
-//   'pink',
-//   'wheat',
-// ]
+window.colors = [
+  'salmon',
+  'crimson',
+  'tomato',
+  'gold',
+  'papayawhip',
+  'peachpuff',
+  'chartreuse',
+  'palegreen',
+  'aquamarine',
+  'turquoise',
+  'skyblue',
+  'plum',
+  'lavander',
+  'pink',
+  'wheat',
+]
 
-function makeBox(color, text) {
+window.makeBox = function makeBox(text, color) {
   const container = document.getElementById('container')
   const newBox = document.createElement('DIV')
   newBox.classList.add('box')
-  if (color) {
-    newBox.style.backgroundColor = color
-  }
+  newBox.style.backgroundColor = color ? color : 'white'
   if (text) {
     newBox.innerText = text
     newBox.style.textAlign = 'center'
   }
   container.appendChild(newBox)
 }
-
-window.makeBox = makeBox
